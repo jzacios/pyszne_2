5845588915505034c487dbe58659639a290d7015
unknown <jakub.zacios@gmail.com>
2020-02-14 09:21:30 +0100

    123

--- assets/skrypty/db.php
+++ assets/skrypty/db.php
@@ -1,3 +1,15 @@
 <?php
 
-require_once("../../skrypty/db.php");
+$servername = "54.38.189.178";
+$username = "pyszne_log";
+$password = "69m02b1C";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=bazunia;charset=utf8", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+?>
