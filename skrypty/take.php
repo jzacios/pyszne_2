2730cb27b6f3f9f2d87a0caea66f6e2bbc8d857c
user <user@v24E.edu.st.poznan.pl>
2020-02-17 15:19:37 +0100

    xD

--- skrypty/take.php
+++ skrypty/take.php
@@ -1,8 +1,13 @@
 <?php
 
-$sql = ""
-
+$sql = "";
+include("db.php");
 session_start();
 $id_zabieram = $_SESSION['user_id'];
 $id_zmiany = $_POST['id'];
-$id_oddaje = $_POST['']
+$id_oddaje;
+
+$sql = ("INSERT INTO `Zmiany_Zmienione` (`Id_zmiany`, `Id_zabiera`, `Id_oddaje`)
+VALUES (?,?,3)");
+$stmt= $conn->prepare($sql);
+$stmt->execute([$id_zmiany, $id_zamieram]);
