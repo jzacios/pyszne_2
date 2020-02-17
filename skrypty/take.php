53e241c77c5cb95362ce299d1e6cb2cf9fc35e55
user <user@v24E.edu.st.poznan.pl>
2020-02-17 15:40:38 +0100

    xd

--- skrypty/take.php
+++ skrypty/take.php
@@ -1,12 +1,15 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 session_start();
 $id_zabieram = $_SESSION['user_id'];
 $id_zmiany = $_POST['id'];
 $id_oddaje;
 
-$sql = ("INSERT INTO `Zmiany_Zmienione` (`Id_zmiany`, `Id_zabiera`, `Id_oddaje`)
+$sql = ("INSERT INTO `Zmiany_zmienione` (`Id_zmiany`, `Id_zabiera`, `Id_oddaje`)
 VALUES (?,?,3)");
 $stmt= $conn->prepare($sql);
 $stmt->execute([$id_zmiany, $id_zabieram]);
