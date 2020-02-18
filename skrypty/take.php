b655f9cd86e4680810e913b44bf77b8b85df787f
unknown <jakub.zacios@gmail.com>
2020-02-18 12:32:23 +0100

    x

--- skrypty/take.php
+++ skrypty/take.php
@@ -4,6 +4,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+include("db.php");
+
 $id_zmiany = $_POST['id'];
 
 $sql = ("SELECT User_ID FROM hours where Shift_id = ?");
@@ -13,8 +15,6 @@ $id_oddaje = $stmt->execute([$id_zmiany])->fetch();
 session_start();
 $id_zabieram = $_SESSION['user_id'];
 $id_oddaje;
-
-include("db.php");
 
 $sql = ("INSERT INTO `Zmiany_zmienione` (`Id_zmiany`, `Id_zabiera`, `Id_oddaje`)
 VALUES (?,?,3)");
