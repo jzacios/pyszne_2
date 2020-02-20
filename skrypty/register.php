279d8f60a7d0baef7244763b7a0dc2c11043f726
user <user@v24E.edu.st.poznan.pl>
2020-02-20 12:04:13 +0100

    xd

--- skrypty/register.php
+++ skrypty/register.php
@@ -1,5 +1,10 @@
 <?php
     
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+
     if(isset($_POST['user']) && isset($_POST['pass'])){
         require_once("db.php");
         session_start();
