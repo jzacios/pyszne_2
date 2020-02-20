cc09017ebcb661690c4d2045bc5ed2d4f8a393d6
user <myzakra@gmail.com>
2020-02-20 14:37:57 +0100

    xD

--- skrypty/register.php
+++ skrypty/register.php
@@ -5,10 +5,18 @@
 
         $log = $_POST['user'];
         $pass = $_POST['password'];
+        $name = $_POST['name'];
+        $surname = $_POST['surname'];
     }
     $sql = ("INSERT INTO `users` (`ID`,`Login`,`Password`,`Privilege`) values (NULL,?,?,1)");
     $stmt = $conn->prepare($sql);
     $stmt->execute([$log, $pass]);
+    
+    $id = $conn->query("SELECT `ID` from `users` where Login ='".$log."'")->fetch();
+    
+    $sql = ("INSERT INTO `workers` (`ID`,`Name`,`Surname`) values (?,?,?)");
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([$id['ID'], $name, $surname]);
     header("Location: ../index.php");
 ?>
 
--- skrypty/register_form.php
+++ skrypty/register_form.php
@@ -14,6 +14,8 @@
                     <a>Zarejestruj!</a>
                     <span class="wrapper_register_login">Login: <input type="text" id="user" name="user"></span><br>
                     <span class="wrapper_register_password">hasło: <input type="text" id="pass" name="password"></span>
+                    <span class="wrapper_register_name">Imie: <input type="text" id="name" name="name"></span><br>
+                    <span class="wrapper_register_surname">Nazwisko: <input type="text" id="surname" name="surname"></span>
                     <Br><span class="wrapper_register_submit"><input type="submit" value="Zarejestruj!"></span>
                 </section>
             </form>
