ff326d7089f39f46e83ce1bfd3eb6858b98c1a54
user <user@v23E.edu.st.poznan.pl>
2020-03-04 09:46:31 +0100

    Bez wpisania poprawnego loginu/hasla nie przejdzie ale nadal jest brzydko wykonane - do zrobienia pozniej

--- skrypty/login.php
+++ skrypty/login.php
@@ -7,17 +7,31 @@
         $_SESSION['password'] = $_POST['password'];
     }
     $user = $conn->query("SELECT ID, Password FROM users WHERE Login = '".$_SESSION['login']."'")->fetch();
-    if($user['Password'] == $_SESSION['password']){
-        $_SESSION['user_id'] = $user['ID'];
-        $_SESSION['login_status'] = 10;
-        echo($user['Password']);
-        echo($_SESSION['password']);
-        header("Location: ../index.php");
+    if(empty(trim($_POST['login']))){
+        echo "upsi";
+    }
+    else if(empty(trim($_POST['password']))){
+        echo "upsi";
+    }
+    else{
+        if($user['Password'] == $_SESSION['password']){
+            $_SESSION['user_id'] = $user['ID'];
+            $_SESSION['login_status'] = 10;
+            echo($user['Password']);
+            echo($_SESSION['password']);
+            header("Location: ../index.php");
 
-    }else{
-        $_SESSION['login_status'] = 0;
-        echo($user['Password']);
-        echo($_SESSION['password']);
-        header("Location: ../index.php");
+        }else if($user['Password'] != $_SESSION['password']){
+            $_SESSION['login_status'] = 0;
+            echo($user['Password']);
+            echo($_SESSION['password']);
+            header("Location: ../index.php");
+        }
+        else{
+            $_SESSION['login_status'] = 0;
+            echo($user['Password']);
+            echo($_SESSION['password']);
+            header("Location: ../index.php");
+        }
     }
 ?>
