388b54bb548775f6f24034a37e1ed1cce8554877
user <jakub.zacios@gmail.com>
2020-03-05 14:20:29 +0100

    xd

--- assets/dodaj.php
+++ assets/dodaj.php
@@ -1,6 +1,6 @@
     <form method="POST" action="../skrypty/send.php">
         <p>Date: <input type="date" name="date"/></p>
-        <p>Start Time: <input type="time" name="times" step="1" min="11:00:00"/></p>
+        <p>Start Time: <input type="time" name="times" step="1" min="11:00:00" max="22:00:00"/></p>
         <p>End Time: <input type="time" name="timee" step="1"/></p>
         <p><input type="submit" value="wyslij"/></p>
         <?php
