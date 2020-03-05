99de4b3dad6fdd6debe179f6c7c3d9debf9d443e
user <myzakra@gmail.com>
2020-03-05 13:37:40 +0100

    xD

--- skrypty/register_form.php
+++ skrypty/register_form.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="../css/login.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
         <title>heheszki w orzeszki</title>
     </head>
     <body>
@@ -12,9 +13,10 @@
             <form action="register.php" method="post">
                 <section class="wrapper_login">
                     <a>Zarejestruj!</a>
-                    <span class="wrapper_register_login">Login: <input type="text" id="user" name="user"></span><br>
+                    <span><i class="fa fa-user-o"></i>
+                    <span class="wrapper_register_login">Login: <input type="text" id="user" name="user"></span></span>
                     <span class="wrapper_register_password">hasło: <input type="password" id="pass" name="password"></span>
-                    <span class="wrapper_register_name">Imie: <input type="text" id="name" name="name"></span><br>
+                    <span class="wrapper_register_name">Imie: <input type="text" id="name" name="name"></span>
                     <span class="wrapper_register_surname">Nazwisko: <input type="text" id="surname" name="surname"></span>
                     <Br><span class="wrapper_register_submit"><input type="submit" value="Zarejestruj!"></span>
                 </section>
