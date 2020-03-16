75aceff5261c735e97193d1b7234b383b3659101
jzacios <jakub.zacios@gmail.com>
2020-03-16 13:47:40 +0100

    cf

--- assets/dodaj.php
+++ assets/dodaj.php
@@ -1,4 +1,8 @@
-    <form method="POST" action="../skrypty/send.php">
+   <?php
+        echo "Dodaj zmiane";
+
+    ?>  
+   <form method="POST" action="../skrypty/send.php">
         <p>Date: <input type="date" name="date" id="data" min="<?php echo date("Y-m-d");  ?>"/></p>
         <p>Start Time: <input type="time" name="times" step="1" min="11:00:00" max="22:00:00"/></p>
         <p>End Time: <input type="time" name="timee" step="1"min="11:00:00" max="22:00:00" /></p>
